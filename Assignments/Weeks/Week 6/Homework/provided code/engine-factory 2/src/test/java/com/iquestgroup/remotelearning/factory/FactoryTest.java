32e682819b9247c77466bf195378f945fe638a7a
Beniamin Savu <benisavu@gmail.com>
2017-06-27 08:08:47 +0300

    Initial commit with diffrent problems

---
+++ Assignments/Weeks/Week 6/Homework/provided code/engine-factory 2/src/test/java/com/iquestgroup/remotelearning/factory/FactoryTest.java
@@ -0,0 +1,17 @@
+package com.iquestgroup.remotelearning.factory;
+
+import com.iquestgroup.remotelearning.domain.Employee;
+import com.iquestgroup.remotelearning.domain.Engine;
+import com.iquestgroup.remotelearning.exception.UnqualifiedEmployeeException;
+import com.iquestgroup.remotelearning.service.EmployeeService;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FactoryTest {
+
+
+}
---
+++ Assignments/Weeks/Week 6/Homework/provided code/engine-factory/src/test/java/com/iquestgroup/remotelearning/factory/FactoryTest.java
@@ -0,0 +1,19 @@
+package com.iquestgroup.remotelearning.factory;
+
+import com.iquestgroup.remotelearning.domain.Employee;
+import com.iquestgroup.remotelearning.domain.Engine;
+import com.iquestgroup.remotelearning.domain.EngineComponent;
+import com.iquestgroup.remotelearning.exception.InsufficientStockException;
+import com.iquestgroup.remotelearning.exception.UnauthorizedEmployeeException;
+import com.iquestgroup.remotelearning.exception.UnqualifiedEmployeeException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FactoryTest {
+
+}
